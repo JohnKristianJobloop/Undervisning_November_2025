a6abc051fe61867266e246a96f499c05265e6234
johnb08 <johnb08.kodehode@gmail.com>
2025-11-04 13:06:27 +0100

    started Stack Implementation, added new Class Library Prosject

---
+++ StackImplementation_November/Models/KhStack.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace StackImplementation_November.Models;
+
+public class KhStack<T>
+{
+    private T[] _internalCollection = [];
+
+    private int _position = 0;
+
+    
+    public void Push(T obj)
+    {
+        if (_position > _internalCollection.Length - 1)
+        {
+            _internalCollection = _growInteralCollection();
+        }
+        /* Her skjer incrementing (++) etter index access */
+        _internalCollection[_position++] = obj;
+    }
+
+    /* Her skjer decrementing (--) før index access */
+    public T Pop() => _internalCollection[--_position];
+
+    private T[] _growInteralCollection()
+    {
+        var counter = Enumerable.Range(0, _position);
+
+        T[] newArray = [.. counter.Select(index => index < _position && _internalCollection[index] is not null ? _internalCollection[index] : default!)];
+
+        return newArray;
+    }
+}
